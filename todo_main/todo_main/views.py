d9dca13ae87abd7c110056867df6090a356e5325
Samuel Adelakun <93160015+nukimoya@users.noreply.github.com>
2024-04-02 02:37:31 -0700

    update on tasks

--- todo_main/todo_main/views.py
+++ todo_main/todo_main/views.py
@@ -1,6 +1,10 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from django.shortcuts import render
-
+from todo.models import Task
 
 def home(request):
+    tasks = Task.objects.filter(is_completed = False)
+    context = {
+        'tasks': tasks,
+    }
     return render(request, 'home.html')
